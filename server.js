411f700eb396664c8243dc97dfce2f4084dcb73a
iPalashAcharya <palashacharya2004@gmail.com>
2025-07-24 18:21:00 +0530

    latest commit

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ env.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5174',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.set('trust proxy', true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
